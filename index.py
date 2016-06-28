84819c68d7b2c63bf12b5ab1a55214eba1192328
williamjohngardner <bill@williamjohngardner.com>
2016-06-28 17:17:13 -0400

    Creating functions

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import requests
 
-
 choice = int(input('''
     ***STAR WARS SWAPI DATABASE***
     Enter a Number to Explore SWAPI
@@ -9,20 +8,74 @@ choice = int(input('''
     3 - List of Vehicles
 '''))
 
+
+
 if choice == 1:
   url = "http://swapi.co/api/people"
   response = requests.get(url).json()
-  for person in response["results"]:
-      print(person["name"])
+  if response['next']:
+      while response['next']:
+        for item in response["results"]:
+            index = item["url"].split('/')
+            url_index = index[-2]
+            print(url_index, item["name"])
+        url = response['next']
+        response = requests.get(url).json()
+      else:
+          for item in response["results"]:
+              index = item["url"].split('/')
+              url_index = index[-2]
+              print(url_index, item["name"])
+  new_choice = int(input("Enter ID Number To See Information on Person: "))
+  url = "http://swapi.co/api/people"
+  new_url = "{}/{}".format(url, new_choice)
+  response = requests.get(new_url).json()
+  for key, value in response.items():
+      print(key, value)
 
 if choice == 2:
   url = "http://swapi.co/api/films"
   response = requests.get(url).json()
-  for film in response["results"]:
-      print(film["title"])
+  if response['next']:
+      while response['next']:
+          for item in response["results"]:
+              index = item["url"].split('/')
+              url_index = index[-2]
+              print(url_index, item["title"])
+          url = response['next']
+          response = requests.get(url).json()
+      else:
+          false
+  for item in response["results"]:
+      index = item["url"].split('/')
+      url_index = index[-2]
+      print(url_index, item["title"])
+  new_choice = int(input("Enter ID Number To See Information on the Film: "))
+  url = "http://swapi.co/api/films"
+  new_url = "{}/{}".format(url, new_choice)
+  response = requests.get(new_url).json()
+  for key, value in response.items():
+      print(key, value)
 
 if choice == 3:
   url = "http://swapi.co/api/vehicles"
   response = requests.get(url).json()
-  for vehicle in response["results"]:
-      print(vehicle["name"])
+  if response['next']:
+      while response['next']:
+          for item in response["results"]:
+              index = item["url"].split('/')
+              url_index = index[-2]
+              print(url_index, index["name"])
+          url = response['next']
+          response = requests.get(url).json()
+      else:
+          for item in response["results"]:
+              index = item["url"].split('/')
+              url_index = index[-2]
+              print(url_index, item["name"])
+  new_choice = int(input("Enter ID Number To See Information on Vehicle: "))
+  url = "http://swapi.co/api/vehicles"
+  new_url = "{}/{}".format(url, new_choice)
+  response = requests.get(new_url).json()
+  for key, value in response.items():
+      print(key, value)
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import requests
 
+
+def get_pagination(response):
+    if response['next']:
+        while response['next']:
+            for item in response["results"]:
+                index = item["url"].split('/')
+                url_index = index[-2]
+                print(url_index, item["name"])
+            url = response['next']
+            response = requests.get(url).json()
+        else:
+            for item in response["results"]:
+                index = item["url"].split('/')
+                url_index = index[-2]
+                print(url_index, item["name"])
+
+
+def new_url(url, new_choice):
+    new_url = "{}/{}".format(url, new_choice)
+    response = requests.get(new_url).json()
+    for key, value in response.items():
+        return key, value
+
+
 choice = int(input('''
     ***STAR WARS SWAPI DATABASE***
     Enter a Number to Explore SWAPI
@@ -13,69 +37,34 @@ choice = int(input('''
 if choice == 1:
   url = "http://swapi.co/api/people"
   response = requests.get(url).json()
-  if response['next']:
-      while response['next']:
-        for item in response["results"]:
-            index = item["url"].split('/')
-            url_index = index[-2]
-            print(url_index, item["name"])
-        url = response['next']
-        response = requests.get(url).json()
-      else:
-          for item in response["results"]:
-              index = item["url"].split('/')
-              url_index = index[-2]
-              print(url_index, item["name"])
-  new_choice = int(input("Enter ID Number To See Information on Person: "))
+  get_pagination(response)
+
+  new_choice = int(input("Enter ID Number To See More Information: "))
   url = "http://swapi.co/api/people"
-  new_url = "{}/{}".format(url, new_choice)
-  response = requests.get(new_url).json()
-  for key, value in response.items():
-      print(key, value)
+  new_url = new_url(url, new_choice)
+  for item in new_url:
+    print(item)
 
 if choice == 2:
   url = "http://swapi.co/api/films"
   response = requests.get(url).json()
-  if response['next']:
-      while response['next']:
-          for item in response["results"]:
-              index = item["url"].split('/')
-              url_index = index[-2]
-              print(url_index, item["title"])
-          url = response['next']
-          response = requests.get(url).json()
-      else:
-          false
   for item in response["results"]:
       index = item["url"].split('/')
       url_index = index[-2]
       print(url_index, item["title"])
-  new_choice = int(input("Enter ID Number To See Information on the Film: "))
+
+  new_choice = int(input("Enter ID Number To See More Information: "))
   url = "http://swapi.co/api/films"
-  new_url = "{}/{}".format(url, new_choice)
-  response = requests.get(new_url).json()
-  for key, value in response.items():
-      print(key, value)
+  new_url(url, new_choice)
+  print(key, value)
 
 if choice == 3:
-  url = "http://swapi.co/api/vehicles"
-  response = requests.get(url).json()
-  if response['next']:
-      while response['next']:
-          for item in response["results"]:
-              index = item["url"].split('/')
-              url_index = index[-2]
-              print(url_index, index["name"])
-          url = response['next']
-          response = requests.get(url).json()
-      else:
-          for item in response["results"]:
-              index = item["url"].split('/')
-              url_index = index[-2]
-              print(url_index, item["name"])
-  new_choice = int(input("Enter ID Number To See Information on Vehicle: "))
-  url = "http://swapi.co/api/vehicles"
-  new_url = "{}/{}".format(url, new_choice)
-  response = requests.get(new_url).json()
-  for key, value in response.items():
-      print(key, value)
+    print("choice3")
+    url = "http://swapi.co/api/vehicles"
+    response = requests.get(url).json()
+    get_pagination(response)
+
+    new_choice = int(input("Enter ID Number To See More Information: "))
+    url = "http://swapi.co/api/vehicles"
+    new_url(url, new_choice)
+    print(key, value)
