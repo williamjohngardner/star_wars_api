6b49f404f52cdab0d706429038f687311e9d6542
williamjohngardner <bill@williamjohngardner.com>
2016-06-28 08:42:09 -0400

    Still no secondary searching

--- main.py
+++ main.py
@@ -10,24 +10,26 @@ choice = int(input('''
 
 if choice == 1:
   url = "http://swapi.co/api/people"
+  url_index = ""
   response = requests.get(url).json()
   if response['next']:
       while response['next']:
         for person in response["results"]:
             index = person["url"].split('/')
-            print(index[-2])
+            url_index = index[-2]
+            print(person["url"])
             print(person["name"])
-            # print(person["url"])
         url = response['next']
         response = requests.get(url).json()
       else:
           for person in response["results"]:
               index = person["url"].split('/')
-              print(index[-2])
+              url_index = index[-2]
+              print(person["url"])
               print(person["name"])
   new_choice = int(input("Enter Number: "))
-# from here i need to find a way to take the index variable above and append it
-# to the url to show the API data for the aforementioned page.
+  #print(url/{}.format(url_index)
+
 
 if choice == 2:
   url = "http://swapi.co/api/films"
